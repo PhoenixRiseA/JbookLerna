6c57c6f1017239353c501ef7bc24ad70d18da03b
sailalith <sailalith.raju@conversight.ai>
2023-11-26 12:15:41 +0530

    lerna implementation but cells api call not working properly

---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+import { createCellsRouter } from "./routes/cells";
+
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app = express();
+  app.use('/cells',createCellsRouter(filename,dir));
+  if (useProxy) {
+    // used to allow development is local machine
+    app.use(
+      createProxyMiddleware({
+        target: "http://127.0.0.1:3000",
+        ws: true,
+        logLevel: "silent",
+      })
+    );
+  } else {
+    // grab build files for users to access react app when running the cli
+    const packagePath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagePath)));
+  }
+
+  
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on("error", reject);
+  });
+};
